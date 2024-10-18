c4c341cb3c73fa88c6da348c55426c4ee3d12623
Sandra Ramirez-Calero <91927380+sandrarcr@users.noreply.github.com>
2024-10-18 14:39:18 +0200

    4_heterosis_intercepts_regression.R

--- 4_heterosis.R
+++ 4_heterosis.R
@@ -5,7 +5,7 @@
 #   to local extinction: low adaptation and low adaptability to marine heat waves.
 #
 #This script contains information of how heterosis was explored in the data
-#This script needs to be run after 2_PCAs_GLM.R and 2_microsat_analysis
+#This script needs to be run after 2_PCAs_GLM.R and 3_microsat_analysis
 #Cleaned data is provided in the repository. cleaning process was conducted to match individuals that
 #had a microsatellite present and that obtained an intercept after making sure necrosis data was present for the three years only
 #individuals with data for two years only were removed.
@@ -25,54 +25,55 @@ setwd(~)
 
 #load data
 #data matched between individuals that have microsatellites and intercepts
-pcla_data <- (read.csv(file = "P.clavata_heterosis_clean.csv", row.names = "Ind"))
-intercepts <- (read.csv(file = "Intercepts_ind_3years_clean.csv", row.names = "ind"))
+dat2<-read.csv("Pcla_interc_het.csv")
+head(dat2)
+
+reg1<-lm(Intercepts+1~MLH,data=dat2)
+summary(reg1)
+plot(dat2$Intercepts+1,dat2$MLH,ylab="nec-int",xlab="sMLH")
+abline(reg1,col="red")
+par(mfrow=c(2,2))
+plot(reg1)
+shapiro.test(residuals(reg1))
+
+require(MASS)
+Box<-MASS::boxcox(reg1,lambda=seq(-6,6,0.0001))
+Cox<-data.frame(Box$x,Box$y)
+Cox2<-Cox[with(Cox, order(-Cox$Box.y)),]
+Cox2[1,]
+lambda<-Cox2[1,"Box.x"] #lambda=0.396
+int_trans<-((dat2$Intercepts+1)^lambda-1)/lambda
+reg2<-lm(int_trans~MLH,data=dat2)
+
+summary(reg2)
+par(mfrow=c(2,2))
+plot(reg2)
+shapiro.test(residuals(reg2))
+plot(dat2$MLH,int_trans,ylab="Intercepts transformadas",
+     xlab="Heterocigosidad")
+plot(dat2$MLH,int_trans,ylab="nec-int",xlab="sMLH")
+plot(int_trans,dat2$MLH,ylab="sMLH",xlab="nec-int")
+abline(reg2,col="red")
+
+#Regresión por Monte-Carlo
+slope_null<-c()
+for (i in 1:10000) {
+	x<-sample(dat2$MLH,71,replace=T)
+	y<-sample(int_trans,71,replace=T)
+	mod<-lm(y~x)
+	slope_null[i]<-mod$coefficients[2]
+}
+lower<-which(slope_null<=reg2$coefficients[2])
+length(lower)/10000 # p = 0.3147
+ordered_null<-order(slope_null)
+ordered_null2<-slope_null[ordered_null]
+ordered_null2[250] #-0.8670218
+hist(slope_null)
+abline(v=reg2$coefficients[2],lty=2,lwd=2,col="red")
+abline(v=-0.8196676,lwd=2,col="red")
+legend("topright",c("alpha = 0.025","observed slope"),
+       lty=c(1,2),lwd=c(2,2),col=c("red","red"),bty="n")
 
 
-
-#obtain Multilocus heterozigosity
-het <- sMLH(pcla) 
-#conver vector in dataframe:
-het_df <- data.frame(het = het, stringsAsFactors = FALSE)
-# Convert het_df dataframe to a format that ggplot2 can use
-het_df <- data.frame(colony = rownames(het_df), MLH = het_df[,1])
-
-# Create a scatter plot with gene names as labels
-ggplot(het_df, aes(x = colony, y = MLH, label = colony)) +
-  geom_point() +
-  geom_text(hjust = 1, vjust = 1, size = 2) +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-  labs(x = "Individuals", y = "sMLH") +
-  theme(panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.border = element_blank(),
-        panel.background = element_blank(),
-        axis.line = element_line(colour = "black"))
-
-#set factor to output data
-het_vect <- het_df$MLH
-#build dataframe
-data <- data.frame(intercepts, het_vect)
-#write.csv(data,file = "pcla_intercept_het_linearR.csv")
-
-#run linear model to test for heterosis and individual phenotypic response
-fit <- lm(het_vect ~ intercepts, data = data)
-summary(fit)
-
-# create scatter plot
-plot(data$intercepts, data$het,
-     xlab = "nec-int",
-     ylab = "sMLH",
-     main = "Multilocus Heterozygosity vs. Phenotypic response",
-     xlim = c(-1.2, 1.2), ylim = c(0, 1.6))
-
-# add trend line
-abline(fit, col = "red")
-
-# add text with p-value and R-squared
-text(x = 0.6, y = 1.5, 
-     labels = paste("R-squared =", round(summary(fit)$r.squared, 1),
-                    "p-value =", round(summary(fit)$coefficients[2, 4], 2)),
-     pos = 3)
 
 ################# End of the code.
